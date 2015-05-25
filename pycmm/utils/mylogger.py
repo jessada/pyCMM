e343c443e4fe59dcc71b51ecdb76b5469dfce823
Jessada <jessada.thutkawkorapin@ki.se>
2015-05-25 12:54:04 +0200

    clean up

--- pycmm/utils/mylogger.py
+++ pycmm/utils/mylogger.py
@@ -1,93 +1,39 @@
 import logging
-#from pycmm import settings
 import sys
 
 lg = logging.getLogger('pycmm')
 lg.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-#fh = logging.FileHandler('spam.log')
-#fh.setLevel(logging.DEBUG)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('## [%(levelname)s]%(asctime)25s - %(processName)-12s - %(name)-12s - %(message)s')
-#fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 # add the handlers to the logger
-#lg.addHandler(fh)
 lg.addHandler(ch)
 
 def set_log_file(file_name, level=logging.DEBUG):
+    # create file handler which logs even debug messages
     fh = logging.FileHandler(file_name)
     fh.setLevel(level)
+    # create formatter and add it to the handlers
     fh.setFormatter(formatter)
+    # add the handlers to the logger
     lg.addHandler(fh)
 
-
-#mylog_file = None
-#logging.basicConfig(level=logging.DEBUG,
-#                    format='## [%(levelname)s]%(asctime)25s - %(processName)-12s - %(name)-12s - %(message)s',
-#                    )
-#lg = logging.getLogger('root')
-#
-#fh = logging.FileHandler('spam.log')
-#fh.setLevel(logging.DEBUG)
-#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#fh.setFormatter(formatter)
-#lg.addHandler(fh)
-#INFO_FMT  = "## [INFO]    {msg}"
-#WARN_FMT  = "## [WARNING] {msg}"
-#DEBUG_FMT = "## [DEBUG]   {msg}"
-#ERROR_FMT = "## [ERROR]   {msg}"
 
 def getLogger(name):
     global lg
     lg = logging.getLogger(name)
 
-#def init(log_file=None,
-#         dbg_mode=False,
-#         ):
-#        mylog_file = log_file
-#        debug_mode=dbg_mode
-
-#def basicConfig(level=logging.DEBUG):
-#    logging.basicConfig(level=level)
-    #logging.basicConfig(filename=filename,level=level)
-
-## **************  defining basic functions  **************
-#def write_log(msg):
-##    if 'mylog_file' in globals():
-#    if mylog_file is not None:
-#        f = open(mylog_file, "a+")
-#        print >> f, msg
-#        f.close()
-
-#def output_msg(msg):
-#    print >> sys.stderr, msg
-#    write_log(msg)
-
 def info(msg):
     lg.info(msg)
-#    formated_msg=INFO_FMT.format(msg=msg)
-#    output_msg(formated_msg)
 
 def warning(msg):
     lg.warning(msg)
-#def warn(msg):
-#    formated_msg=WARN_FMT.format(msg=msg)
-#    output_msg(formated_msg)
 
 def debug(msg):
     lg.debug(msg)
-#    formated_msg=DEBUG_FMT.format(msg=msg)
-#    if debug_mode:
-#        output_msg(formated_msg)
-#    else:
-#        write_log(formated_msg)
 
 def throw(err_msg):
     lg.error(err_msg)
-#    formated_msg=ERROR_FMT.format(msg=err_msg)
-#    write_log(formated_msg)
-#    raise Exception(formated_msg)
