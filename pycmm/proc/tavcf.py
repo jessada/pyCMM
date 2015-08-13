fb6dc2520aec54309e72579b9a03db2a80689128
jessada <jessada.thutkawkorapin@gmail.com>
2015-08-13 15:02:27 +0200

    add cmmgt_type to _Call

--- pycmm/proc/tavcf.py
+++ pycmm/proc/tavcf.py
@@ -1,10 +1,68 @@
-from vcf import Reader as VCFReader
+import itertools
+from vcf import Reader as VcfReader
 from vcf import RESERVED_INFO
 from collections import OrderedDict
 from pycmm.utils import mylogger
 
+try:
+    import cparse
+except ImportError:
+    cparse = None
 
-class TableAnnovarVCFReader(VCFReader):
+#from vcf.model import _Call, _Record, make_calldata_tuple
+from vcf.model import _Call as _VCFCall
+
+CMMGT_WILDTYPE = 'wt'
+CMMGT_HOMOZYGOTE = 'hom'
+CMMGT_HETEROZYGOTE = 'het'
+CMMGT_OTHER = 'oth'
+
+
+class _CmmGt(object):
+    """
+    to calculate type of genotype given allele index
+      - 0 -> REF
+      - 1 -> first ALT
+      - and so on
+    """
+
+    def __init__(self, gt):
+        self.__gt = gt
+
+    def __repr__(self):
+        if (self.__gt == ".") or (self.__gt == "./."):
+            return "."
+        max_allele_idx = max(map(lambda x: int(x),
+                                 self.__gt.split("/")))
+        return str(map(lambda x: self[x], range(max_allele_idx+1)))
+
+    def __getitem__(self, key):
+        if self.__gt == ".":
+            return "."
+        if self.__gt == "./.":
+            return "."
+        gts = self.__gt.split("/")
+        if (gts[0] == "0") and (gts[1] == "0"):
+            return CMMGT_WILDTYPE
+        if (gts[0] == str(key)) or (gts[1] == str(key)):
+            if gts[0] == gts[1]:
+                return CMMGT_HOMOZYGOTE
+            else:
+                return CMMGT_HETEROZYGOTE
+        return CMMGT_OTHER
+
+class _CmmVcfCall(_VCFCall):
+    """
+    An encapsulated version of vcf._Call from pyVCF package to
+      - add extra genotype translation, "cmmgt_type", to handle record
+        with more than one alternate alleles
+    """
+
+    @property
+    def cmmgt_type(self):
+        return _CmmGt(self.data.GT)
+
+class TableAnnovarVcfReader(VcfReader):
     """
     An encapsulated version of vcf.Reader from pyVCF package to fix
       - _parse_info  <see _parse_info function>
@@ -17,7 +75,7 @@ class TableAnnovarVCFReader(VCFReader):
                  prepend_chr=False,
                  strict_whitespace=False,
                  ):
-        super(TableAnnovarVCFReader, self).__init__(fsock=fsock,
+        super(TableAnnovarVcfReader, self).__init__(fsock=fsock,
                                                     filename=filename,
                                                     compressed=compressed,
                                                     prepend_chr=prepend_chr,
@@ -119,4 +177,89 @@ class TableAnnovarVCFReader(VCFReader):
             retdict[ID] = val
 
         return retdict
+
+    def _parse_samples(self, samples, samp_fmt, site):
+        '''Parse a sample entry according to the format specified in the FORMAT
+        column.
+
+        NOTE: this method has a cython equivalent and care must be taken
+        to keep the two methods equivalent
+        '''
+
+        # check whether we already know how to parse this format
+        if samp_fmt not in self._format_cache:
+            self._format_cache[samp_fmt] = self._parse_sample_format(samp_fmt)
+        samp_fmt = self._format_cache[samp_fmt]
+
+        if cparse:
+            return cparse.parse_samples(
+                self.samples, samples, samp_fmt, samp_fmt._types, samp_fmt._nums, site)
+
+        samp_data = []
+        _map = self._map
+
+        nfields = len(samp_fmt._fields)
+
+        for name, sample in itertools.izip(self.samples, samples):
+
+            # parse the data for this sample
+            sampdat = [None] * nfields
+
+            for i, vals in enumerate(sample.split(':')):
+
+                # short circuit the most common
+                if samp_fmt._fields[i] == 'GT':
+                    sampdat[i] = vals
+                    continue
+                elif vals == ".":
+                    sampdat[i] = None
+                    continue
+
+                entry_num = samp_fmt._nums[i]
+                entry_type = samp_fmt._types[i]
+
+                # we don't need to split single entries
+                if entry_num == 1 or ',' not in vals:
+
+                    if entry_type == 'Integer':
+                        try:
+                            sampdat[i] = int(vals)
+                        except ValueError:
+                            sampdat[i] = float(vals)
+                    elif entry_type == 'Float':
+                        sampdat[i] = float(vals)
+                    else:
+                        sampdat[i] = vals
+
+                    if entry_num != 1:
+                        sampdat[i] = (sampdat[i])
+
+                    continue
+
+                vals = vals.split(',')
+
+                if entry_type == 'Integer':
+                    try:
+                        sampdat[i] = _map(int, vals)
+                    except ValueError:
+                        sampdat[i] = _map(float, vals)
+                elif entry_type == 'Float' or entry_type == 'Numeric':
+                    sampdat[i] = _map(float, vals)
+                else:
+                    sampdat[i] = vals
+
+            # create a call object
+            call = _CmmVcfCall(site, name, samp_fmt(*sampdat))
+            if (call.sample == "Br-429") and (call.data.GT == "2/3"):
+                mylogger.debug(sampdat)
+                mylogger.debug(samp_fmt(*sampdat))
+                mylogger.debug(call)
+                mylogger.debug(call.gt_bases)
+                mylogger.debug(call.data.GT)
+                mylogger.debug(call.cmmgt_type)
+            samp_data.append(call)
+
+#        mylogger.debug(len(samp_data))
+#        mylogger.debug(samp_data[1])
+        return samp_data
 
--- pycmm/proc/test/test_tavcf.py
+++ pycmm/proc/test/test_tavcf.py
@@ -3,10 +3,10 @@ from os.path import join as join_path
 from os.path import dirname
 from pycmm.utils import mylogger
 from pycmm.template import SafeTester
-from pycmm.proc.tavcf import TableAnnovarVCFReader
+from pycmm.proc.tavcf import TableAnnovarVcfReader
 
 
-class TestTableAnnovarVCFReader(SafeTester):
+class TestTableAnnovarVcfReader(SafeTester):
 
     def __init__(self, test_name):
         SafeTester.__init__(self,
@@ -28,10 +28,10 @@ class TestTableAnnovarVCFReader(SafeTester):
         self.init_test(self.current_func_name)
         in_file = join_path(self.data_dir,
                                'input.vcf.gz')
-        vcf_reader = TableAnnovarVCFReader(filename=in_file)
+        vcf_reader = TableAnnovarVcfReader(filename=in_file)
         self.assertEqual(len(vcf_reader.annovar_infos),
                          16,
-                         "TableAnnovarVCFReader cannot locate ANNOVAR info")
+                         "TableAnnovarVcfReader cannot locate ANNOVAR info")
 
     def test_annovar_info_2(self):
         """ to check if the property work correctly if no info annotated by ANNOVAR """
@@ -40,10 +40,10 @@ class TestTableAnnovarVCFReader(SafeTester):
         self.init_test(self.current_func_name)
         in_file = join_path(self.data_dir,
                                'input.vcf.gz')
-        vcf_reader = TableAnnovarVCFReader(filename=in_file)
+        vcf_reader = TableAnnovarVcfReader(filename=in_file)
         self.assertEqual(len(vcf_reader.annovar_infos),
                          0,
-                         "TableAnnovarVCFReader cannot locate ANNOVAR info")
+                         "TableAnnovarVcfReader cannot locate ANNOVAR info")
 
     def test_parse_info_1(self):
         """
@@ -55,14 +55,17 @@ class TestTableAnnovarVCFReader(SafeTester):
         self.init_test(self.current_func_name)
         in_file = join_path(self.data_dir,
                                'input.vcf.gz')
-        vcf_reader = TableAnnovarVCFReader(filename=in_file)
+        vcf_reader = TableAnnovarVcfReader(filename=in_file)
         vcf_reader.next()
         vcf_reader.next()
         vcf_reader.next()
         vcf_record = vcf_reader.next()
+        mylogger.debug(vcf_record.FORMAT)
+
+        mylogger.debug(vcf_record.samples)
         self.assertEqual(vcf_record.INFO['Gene.refGene'],
                          "ZNF264,AURKC",
-                         "TableAnnovarVCFReader cannot correctly parse comma-separated info annotated by ANNOVAR")
+                         "TableAnnovarVcfReader cannot correctly parse comma-separated info annotated by ANNOVAR")
 
     def test_parse_info_2(self):
         """ all the hex character shall be converted back to normal character """
@@ -71,13 +74,13 @@ class TestTableAnnovarVCFReader(SafeTester):
         self.init_test(self.current_func_name)
         in_file = join_path(self.data_dir,
                                'input.vcf.gz')
-        vcf_reader = TableAnnovarVCFReader(filename=in_file)
+        vcf_reader = TableAnnovarVcfReader(filename=in_file)
         vcf_reader.next()
         vcf_reader.next()
         vcf_record = vcf_reader.next()
         self.assertEqual(vcf_record.INFO['GeneDetail.refGene'],
                          "dist=6394;dist=1769",
-                         "TableAnnovarVCFReader cannot correctly parse hex-encoded info annotated by ANNOVAR")
+                         "TableAnnovarVcfReader cannot correctly parse hex-encoded info annotated by ANNOVAR")
 
     def test_parse_info_3(self):
         """
@@ -89,19 +92,100 @@ class TestTableAnnovarVCFReader(SafeTester):
         self.init_test(self.current_func_name)
         in_file = join_path(self.data_dir,
                                'input.vcf.gz')
-        vcf_reader = TableAnnovarVCFReader(filename=in_file)
+        vcf_reader = TableAnnovarVcfReader(filename=in_file)
         vcf_reader.next()
         vcf_reader.next()
         vcf_reader.next()
         vcf_record = vcf_reader.next()
         self.assertEqual(vcf_record.INFO['AXEQ_BR_AF'],
                          "0.1170",
-                         "TableAnnovarVCFReader cannot identify value for info entries annotated by ANNOVAR")
+                         "TableAnnovarVcfReader cannot identify value for info entries annotated by ANNOVAR")
         vcf_record = vcf_reader.next()
         self.assertEqual(vcf_record.INFO['AXEQ_BR_AF'],
                          ['0.3068', '0.1818', '0.2614', '0.0341'],
-                         "TableAnnovarVCFReader cannot identify value for info entries annotated by ANNOVAR")
+                         "TableAnnovarVcfReader cannot identify value for info entries annotated by ANNOVAR")
         vcf_record = vcf_reader.next()
         self.assertEqual(vcf_record.INFO['AXEQ_BR_AF'],
                          ['0.2692', '0.2308'],
-                         "TableAnnovarVCFReader cannot identify value for info entries annotated by ANNOVAR")
+                         "TableAnnovarVcfReader cannot identify value for info entries annotated by ANNOVAR")
+
+    def test_parse_cmmgt_type(self):
+        """ test if zygosity can be correctly determined """
+
+        self.individual_debug = True
+        self.init_test(self.current_func_name)
+        in_file = join_path(self.data_dir,
+                               'input.vcf.gz')
+        vcf_reader = TableAnnovarVcfReader(filename=in_file)
+        vcf_reader.next()
+        vcf_reader.next()
+        vcf_reader.next()
+        vcf_reader.next()
+        vcf_record = vcf_reader.next()
+        call = vcf_record.genotype("Br-429")
+        self.assertEqual(call.cmmgt_type[1],
+                         "oth",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[2],
+                         "het",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[3],
+                         "het",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[4],
+                         "oth",
+                         "cmm_genotye cannot be correctly determined")
+        call = vcf_record.genotype("Br-432")
+        self.assertEqual(call.cmmgt_type[1],
+                         "hom",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[2],
+                         "oth",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[3],
+                         "oth",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[4],
+                         "oth",
+                         "cmm_genotye cannot be correctly determined")
+        call = vcf_record.genotype("Al-161")
+        self.assertEqual(call.cmmgt_type[1],
+                         "het",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[2],
+                         "oth",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[3],
+                         "het",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[4],
+                         "oth",
+                         "cmm_genotye cannot be correctly determined")
+        vcf_record = vcf_reader.next()
+        call = vcf_record.genotype("Al-65")
+        self.assertEqual(call.cmmgt_type[1],
+                         ".",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[2],
+                         ".",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[3],
+                         ".",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[4],
+                         ".",
+                         "cmm_genotye cannot be correctly determined")
+        vcf_record = vcf_reader.next()
+        call = vcf_record.genotype("Al-111")
+        self.assertEqual(call.cmmgt_type[1],
+                         "wt",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[2],
+                         "wt",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[3],
+                         "wt",
+                         "cmm_genotye cannot be correctly determined")
+        self.assertEqual(call.cmmgt_type[4],
+                         "wt",
+                         "cmm_genotye cannot be correctly determined")
