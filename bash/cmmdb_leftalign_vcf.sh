b74899795f02039c3d0167b5ad7d8a568c519697
Jessada <jessada.thutkawkorapin@ki.se>
2017-05-04 10:52:11 +0200

    prelim of cmmdb_leftalign_vcf

---
+++ bash/cmmdb_leftalign_vcf.sh
@@ -0,0 +1,116 @@
+#!/bin/bash 
+module load annovar
+source $PYCMM/bash/cmm_functions.sh
+
+script_name=$(basename $0)
+params="$@"
+
+usage=$(
+cat <<EOF
+usage:
+$0 [OPTION]
+option:
+-i {file}         input VCF file (required)
+-o {file}         output file (required)
+-h                this help
+EOF
+)
+
+# parse option
+while getopts ":i:o:h" OPTION; do
+  case "$OPTION" in
+    i)
+      input_vcf="$OPTARG"
+      ;;
+    o)
+      out_file="$OPTARG"
+      ;;
+    h)
+      echo >&2 "$usage"
+      ;;
+    *)
+      die "unrecognized option (-$OPTION) from executing: $0 $@"
+      ;;
+  esac
+done
+
+[ ! -z $input_vcf ] || die "an input VCF file is required (-i)"
+[ ! -z $out_file ] || die "please indicate output file name (-o)"
+[ -f "$input_vcf" ] || die "$input_vcf is not a valid file name."
+
+time_stamp=$( date )
+
+cd $PYCMM
+revision_no=`git rev-list HEAD | wc -l`
+revision_code=`git rev-parse HEAD`
+cd - > /dev/null
+
+## ****************************************  display configuration  ****************************************
+## display required configuration
+new_section_txt "S T A R T <$script_name>"
+info_msg
+info_msg "description"
+info_msg "  decompose and left align vcf file"
+info_msg
+info_msg "version and script configuration"
+display_param "revision no" "$revision_no"
+display_param "revision code" "$revision_code"
+display_param "script path" "$PYCMM"
+display_param "parameters" "$params"
+display_param "time stamp" "$time_stamp"
+info_msg
+info_msg "overall configuration"
+display_param "input VCF file (-i)" "$input_vcf"
+display_param "output file (-o)" "$out_file"
+display_param "working directory" "$working_dir"
+
+# ****************************************  executing  ****************************************
+ 
+new_section_txt "E X E C U T I N G"
+
+function decompose_n_leftalign {
+    chrom=$1
+    # decompose and leftalign input vcf
+    cmd="tabix -h"
+    cmd+=" $input_vcf"
+    cmd+=" $chrom"
+    cmd+=" | vt decompose -s -"
+    cmd+=" | vt normalize -r $GRCH37_REF -"
+    cmd+=" | grep -v \"^#\""
+    cmd+=" >> $out_file"
+    eval_cmd "$cmd"
+}
+
+tabix -h $input_vcf_gz 1:1-1 > $out_file
+decompose_n_leftalign 1
+decompose_n_leftalign 2
+decompose_n_leftalign 3
+decompose_n_leftalign 4
+decompose_n_leftalign 5
+decompose_n_leftalign 6
+decompose_n_leftalign 7
+decompose_n_leftalign 8
+decompose_n_leftalign 9
+decompose_n_leftalign 10
+decompose_n_leftalign 11
+decompose_n_leftalign 12
+decompose_n_leftalign 13 
+decompose_n_leftalign 14
+decompose_n_leftalign 15
+decompose_n_leftalign 16
+decompose_n_leftalign 17
+decompose_n_leftalign 18
+decompose_n_leftalign 19
+decompose_n_leftalign 20
+decompose_n_leftalign 21
+decompose_n_leftalign 22
+decompose_n_leftalign "X"
+decompose_n_leftalign "Y"
+
+cmd="bgzip -f $out_file"
+eval_cmd "$cmd"
+
+cmd="tabix -p vcf $out_file.gz"
+eval_cmd "$cmd"
+
+new_section_txt "F I N I S H <$script_name>"
